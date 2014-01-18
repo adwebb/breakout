b78ccbe8dead8f7c8053edc0a9a7ff7151f76109
Andrew Webb <adwebb@gmail.com>
2014-01-17 20:19:41 -0600

    super mario breakout

---
+++ Pong/CoinView.h
@@ -0,0 +1,13 @@
+//
+//  CoinView.h
+//  Breakout
+//
+//  Created by Andrew Webb on 1/17/14.
+//  Copyright (c) 2014 Andrew Webb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoinView : UIView
+
+@end
---
+++ Pong/CoinView.m
@@ -0,0 +1,31 @@
+//
+//  CoinView.m
+//  Breakout
+//
+//  Created by Andrew Webb on 1/17/14.
+//  Copyright (c) 2014 Andrew Webb. All rights reserved.
+//
+
+#import "CoinView.h"
+
+@implementation CoinView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
