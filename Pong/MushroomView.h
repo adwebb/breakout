c16cdd2fff0a55009b5b93adc4b528fc9fc91c86
Andrew Webb <adwebb@gmail.com>
2014-01-19 23:47:12 -0600

    mushrooms and underworld added

---
+++ Pong/MushroomView.h
@@ -0,0 +1,13 @@
+//
+//  mushroomView.h
+//  Breakout
+//
+//  Created by Andrew Webb on 1/18/14.
+//  Copyright (c) 2014 Andrew Webb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mushroomView : UIView
+
+@end
---
+++ Pong/MushroomView.m
@@ -0,0 +1,31 @@
+//
+//  mushroomView.m
+//  Breakout
+//
+//  Created by Andrew Webb on 1/18/14.
+//  Copyright (c) 2014 Andrew Webb. All rights reserved.
+//
+
+#import "mushroomView.h"
+
+@implementation mushroomView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
